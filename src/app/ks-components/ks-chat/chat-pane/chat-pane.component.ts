2b9f8e39068c82d68ae82d6b9f0c33bd003cfaaf
kostamas <kosta.masliansky@gmail.com>
2017-11-26 13:06:49 +0200

    Kosta/img zoomer (#18)
    * save
    
    * save
    
    * save
    
    * save
    
    * image expander
    
    * ui fixes
    
    * add user name to chat message

--- src/app/ks-components/ks-chat/chat-pane/chat-pane.component.ts
+++ src/app/ks-components/ks-chat/chat-pane/chat-pane.component.ts
@@ -26,8 +26,9 @@ export class ChatPaneComponent implements OnInit, AfterViewChecked {
     this.actions.scrollToBottom = this.scrollToBottom;
   }
 
-  public keyPressHandler(keyCode) {
-    if (keyCode === 13 && this.message.length > 0) {
+  public keyPressHandler($event) {
+    if ($event.keyCode === 13 && this.message.length > 0) {
+      $event.preventDefault();
       this.chatService.updateMessages(this.message, this.activeChatter.chat, this.localUser);
       this.message = '';
     }
