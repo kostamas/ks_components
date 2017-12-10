218b132b2a398c59c55433db65b2b2f1b0cab65a
kostamas <kosta.masliansky@gmail.com>
2017-12-10 15:00:28 +0200

    css fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,7 @@ import {TranslateService} from '@ngx-translate/core';
   template: `
     <div class="home-navigation" routerLink="/home" [routerLinkActive]="['home']">Home</div>
     <router-outlet></router-outlet>`,
-  styles: [`.home-navigation {
-    width: 80px;
-    height: 40px;
-    background-color: #22d470;
-    box-shadow: 0 0 14px 0 rgba(0, 0, 0, 0.75);
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    left: 50px;
-    top: 20px;
-    position: absolute;
-    font-size: 19px;
-    z-index: 10;
-  }
-
-  .home-navigation:hover {
-    opacity: 0.8;
-  }
-
-  .home-navigation.home {
-    display: none;
-  }
-  `]
+  styleUrls:['./app.component.scss']
 })
 export class AppComponent {
 
