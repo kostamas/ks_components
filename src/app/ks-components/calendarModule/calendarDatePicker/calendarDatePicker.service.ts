148b46bc7978a9ec1751c0a15a3c11d5e6cb1b7a
Kosta Masliansky <kosta.masliansky@hotelbeds.com>
2018-11-28 15:15:09 +0200

    save

--- src/app/ks-components/calendarModule/calendarDatePicker/calendarDatePicker.service.ts
+++ src/app/ks-components/calendarModule/calendarDatePicker/calendarDatePicker.service.ts
@@ -1,18 +1,34 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
+import * as moment from "moment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarDatePickerService {
+  public DATE_FORMAT: string = 'YYYY-MM-DD';
   public selectDate$: any = new Subject();
-  public selectedRange: any = {firsDate: null, lastDate: null};
+  public selectedRange: any = {date1: null, date2: null};
 
   constructor() {
   }
 
   clearSelectRange(): void {
-    this.selectedRange = {firsDate: null, lastDate: null};
+    this.selectedRange = {date1: null, date2: null};
     this.selectDate$.next();
+  }
+
+  getSelectedRange(): any {
+    let lastDate, firstDate;
+    if (this.selectedRange.date2) {
+      firstDate = this.selectedRange.date1.diff(this.selectedRange.date2) < 0 ? moment(this.selectedRange.date1) : moment(this.selectedRange.date2);
+      lastDate = this.selectedRange.date1.diff(this.selectedRange.date2) > 0 ? moment(this.selectedRange.date1) : moment(this.selectedRange.date2);
+    } else {
+      if (this.selectedRange.date1) {
+        firstDate = moment(this.selectedRange.date1);
+        lastDate = moment(this.selectedRange.date1);
+      }
+    }
+    return {firstDate, lastDate};
   }
 }
--- src/app/ks-components/calendarModule/multiDatePicker/multiDatePicker.component.ts
+++ src/app/ks-components/calendarModule/multiDatePicker/multiDatePicker.component.ts
@@ -1,8 +1,12 @@
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild, ViewEncapsulation} from '@angular/core';
 import * as moment from 'moment';
-import {fromEvent, Subject} from 'rxjs';
-import {filter, skip, switchMap, takeUntil, tap} from 'rxjs/operators';
+import {fromEvent, merge, Subject} from 'rxjs';
+import {filter, switchMap, takeUntil, tap} from 'rxjs/operators';
 import {CalendarDatePickerService} from '../calendarDatePicker/calendarDatePicker.service';
+import {SVG_ICONS} from "../../svgIconModule/svg-icons.const";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AbstractControl} from '@angular/forms';
+import {isEmpty} from "../../../utils/jsUtils";
 
 @Component({
   selector: 'app-multi-date-picker',
@@ -13,31 +17,55 @@ import {CalendarDatePickerService} from '../calendarDatePicker/calendarDatePicke
 export class MultiDatePickerComponent implements OnInit, OnDestroy {
   public daysToSelect: any;
   public selection$: any = new Subject();
+  public cancelSelection$: any = new Subject();
   public moment: any = moment;
   public calendarDates: string[];
   public unSubscribe$: any = new Subject();
-  public DATE_FORMAT: string = 'YYYY-MM-DD';
-  public canSelect: boolean;  // todo - find better solution
+  public DATE_INPUT_FORMAT: string = 'DD/MM/YYYY';
+  public canSelect: boolean;
+  public SVG_ICONS: any = SVG_ICONS;
+  public formGroup: FormGroup;
+  private rgx = new RegExp('[0-9]{2}/[0-9]{2}/20[0-9]{2}');
+  private RANGE_SIZE = 31;
 
   @ViewChild('multiDatePicker') multiDatePicker: any;
-
-  @Input('config') config: any;
-
+  @Input('config') config?: any;
   @Output('onSelectRange') onSelectRange: EventEmitter<any> = new EventEmitter();
 
-  constructor(private calendarDatePickerService: CalendarDatePickerService) {
+  constructor(private calendarDatePickerService: CalendarDatePickerService, private fBuilder: FormBuilder) {
+    this.formGroup = fBuilder.group({
+      from: [null, this.dateFormatValidator],
+      to: [null, this.dateFormatValidator]
+    }, {validator: this.rangeValidation});
   }
 
   ngOnInit(): void {
     const now = moment();
     this.calendarDates = [
-      now.format(this.DATE_FORMAT),
-      now.add(1, 'month').format(this.DATE_FORMAT),
-      now.add(1, 'month').format(this.DATE_FORMAT)
+      now.format(this.calendarDatePickerService.DATE_FORMAT),
+      now.add(1, 'month').format(this.calendarDatePickerService.DATE_FORMAT),
+      now.add(1, 'month').format(this.calendarDatePickerService.DATE_FORMAT)
     ];
 
     this.initMouseEventsHandler();
     this.initSelectedRangeHandler();
+
+    this.formGroup.valueChanges.subscribe(() => setTimeout(() => this.rangeFromInputHandler()));
+
+    if (this.config && this.config.selectedRange) {
+      setTimeout(() => {
+        const calendarFirstDate = moment(this.config.selectedRange.firstDate);
+        this.calendarDates = [
+          calendarFirstDate.format(this.calendarDatePickerService.DATE_FORMAT),
+          calendarFirstDate.add(1, 'month').format(this.calendarDatePickerService.DATE_FORMAT),
+          calendarFirstDate.add(1, 'month').format(this.calendarDatePickerService.DATE_FORMAT)
+        ];
+        const {firstDate, lastDate} = this.config.selectedRange;
+        this.calendarDatePickerService.selectedRange = {date1: moment(firstDate), date2: moment(lastDate)};
+        this.calendarDatePickerService.selectDate$.next(moment(firstDate));
+        this.calendarDatePickerService.selectDate$.next(moment(lastDate));
+      })
+    }
   }
 
   initMouseEventsHandler(): void {
@@ -65,7 +93,12 @@ export class MultiDatePickerComponent implements OnInit, OnDestroy {
 
   switchMapHandler = () => {
     return fromEvent(this.multiDatePicker.nativeElement, 'mousemove')
-      .pipe(takeUntil(fromEvent(this.multiDatePicker.nativeElement, 'click')));
+      .pipe(takeUntil(
+        merge(
+          this.cancelSelection$,
+          fromEvent(this.multiDatePicker.nativeElement, 'click'))
+        )
+      );
   };
 
   initSelectedRangeHandler(): void {
@@ -74,55 +107,133 @@ export class MultiDatePickerComponent implements OnInit, OnDestroy {
       .subscribe((selectedDate: any) => {
         const {selectedRange} = this.calendarDatePickerService;
 
-        if (!selectedRange.firsDate) {
-          selectedRange.firsDate = selectedDate;
-          selectedRange.lastDate = selectedDate;
+        if (!selectedRange.date1) {
+          selectedRange.date1 = selectedDate;
         } else {
-          if (selectedRange.lastDate && selectedDate.diff(selectedRange.firsDate) < 0) {
-            selectedRange.firsDate = selectedDate;
-          } else {
-            selectedRange.lastDate = selectedDate;
-          }
+          selectedRange.date2 = Math.abs(selectedRange.date1.diff(selectedDate, 'days')) < this.RANGE_SIZE ? selectedDate : selectedRange.date2;
         }
-
-        if (selectedRange.lastDate && selectedRange.lastDate.diff(selectedRange.firsDate) < 0) {
-          const firstDay = selectedRange.firsDate;
-          selectedRange.firsDate = selectedRange.lastDate;
-          selectedRange.lastDate = firstDay;
-        }
+        this.updateDateInput();
       });
   }
 
   updateCalendarDates(direction: number): void {
-    const format = 'YYYY-MM-DD';
     const currentViewDate = moment(this.calendarDates[0]);
+    const {calendarDatePickerService} = this;
 
     if (direction > 0) {
       this.calendarDates = [
-        currentViewDate.add(direction, 'month').format(format),
-        currentViewDate.add(direction, 'month').format(format),
-        currentViewDate.add(direction, 'month').format(format)
+        currentViewDate.add(direction, 'month').format(calendarDatePickerService.DATE_FORMAT),
+        currentViewDate.add(direction, 'month').format(calendarDatePickerService.DATE_FORMAT),
+        currentViewDate.add(direction, 'month').format(calendarDatePickerService.DATE_FORMAT)
       ];
     } else {
       this.calendarDates[2] = this.calendarDates[1];
       this.calendarDates[1] = this.calendarDates[0];
-      this.calendarDates[0] = currentViewDate.add(direction, 'month').format(format);
+      this.calendarDates[0] = currentViewDate.add(direction, 'month').format(calendarDatePickerService.DATE_FORMAT);
     }
   }
 
   select(): void {
-    const {selectedRange} = this.calendarDatePickerService;
-    if (selectedRange.firsDate && selectedRange.lastDate) {
-      console.log(selectedRange.firsDate.format('MMMM Do YYYY') + ' - ' + selectedRange.lastDate.format('MMMM Do YYYY'));
-    }
+    const {firstDate, lastDate} = this.calendarDatePickerService.getSelectedRange();
+    const  format = this.config.format || 'MMMM Do YYYY';
     this.onSelectRange.next({
-      from: moment(selectedRange.firsDate).format(this.config.format || 'MMMM Do YYYY'),
-      to: moment(selectedRange.lastDate).format(this.config.format || 'MMMM Do YYYY')
+      from: firstDate ? firstDate.format(format) : moment().format(format),
+      to: lastDate ? lastDate.format(format) : moment().add(1, 'day').format(format)
     });
   }
 
   cancel(): void {
+    this.canSelect = false;
+    this.formGroup.setValue({from: '', to: ''});
+    this.cancelSelection$.next();
     this.calendarDatePickerService.clearSelectRange();
+  }
+
+  rangeFromInputHandler() {
+    const {from, to} = this.formGroup.controls;
+    const {firstDate, lastDate} = this.calendarDatePickerService.getSelectedRange();
+    const {DATE_FORMAT} = this.calendarDatePickerService;
+
+    if (!isEmpty(from.errors) || !isEmpty(to.errors) || !from.value || !to.value) {
+      return;
+    }
+
+    const newFrom = this.fixFormat(from.value);
+    const newTo = this.fixFormat(to.value);
+    let date = moment(this.fixFormat(from.value));
+
+    if (newFrom === moment(firstDate).format(DATE_FORMAT) && newTo === moment(lastDate).format(DATE_FORMAT)) {
+      return
+    }
+
+    this.calendarDates = [
+      date.format(this.calendarDatePickerService.DATE_FORMAT),
+      date.add(1, 'month').format(this.calendarDatePickerService.DATE_FORMAT),
+      date.add(1, 'month').format(this.calendarDatePickerService.DATE_FORMAT)
+    ];
+
+    this.calendarDatePickerService.selectedRange = {date1: moment(newFrom), date2: moment(newTo)};
+    this.calendarDatePickerService.selectDate$.next(moment(newFrom));
+    this.calendarDatePickerService.selectDate$.next(moment(newTo));
+  }
+
+  updateDateInput() {
+    const {firstDate, lastDate} = this.calendarDatePickerService.getSelectedRange();
+    const from = !!firstDate ? firstDate.format(this.DATE_INPUT_FORMAT) : '';
+    const to = !!lastDate ? lastDate.format(this.DATE_INPUT_FORMAT) : '';
+    this.formGroup.setValue({from, to});
+  }
+
+  private fixFormat(date: string) { // todo - temporary fix
+    return date.split('/').reverse().join('-');
+  }
+
+  dateFormatValidator = (ctrl: AbstractControl) => (!ctrl.value || this.rgx.test(ctrl.value)) ? null : {'format-error': true};
+
+  // validate from range
+  rangeValidation = (abstractForm: FormGroup) => { // todo - need find a better way
+    const {from, to} = abstractForm.controls;
+
+    if (from.value) {
+      const fromDateMilliseconds = moment(this.fixFormat(from.value)).valueOf();
+      const todayMilliseconds = moment(moment().format(this.calendarDatePickerService.DATE_FORMAT)).valueOf();
+      const MILLI_SECONDS_IN_DAY = 86400000;
+
+      if ((fromDateMilliseconds / MILLI_SECONDS_IN_DAY) < (todayMilliseconds / MILLI_SECONDS_IN_DAY)) {
+        this.formGroup.controls['from'].setErrors({'wrong-range': true});
+        return;
+      }
+
+      let toError = this.formGroup.controls['to'].errors;
+      if (toError && toError['wrong-range']) {
+        delete toError['wrong-range'];
+      }
+      if (isEmpty(this.formGroup.controls['to'].errors)) {
+        toError = null;
+      }
+      this.formGroup.controls['to'].setErrors(toError);
+
+      if (from.value && to.value) {
+        const toMilliseconds = moment(this.fixFormat(to.value)).valueOf();
+        if ((toMilliseconds / MILLI_SECONDS_IN_DAY) - (fromDateMilliseconds / MILLI_SECONDS_IN_DAY) > this.RANGE_SIZE) { // check range size
+          this.formGroup.controls['to'].setErrors({'wrong-range': true});
+          return
+        }
+
+        if (toMilliseconds < fromDateMilliseconds) {
+          this.formGroup.controls['to'].setErrors({'wrong-range': true});
+          return;
+        }
+      }
+    }
+    return null;
+  };
+
+  errorsClass(errors) {
+    if (!errors) {
+      return '';
+    }
+    return Object.keys(errors).join(' ');
   }
 
   ngOnDestroy(): void {
